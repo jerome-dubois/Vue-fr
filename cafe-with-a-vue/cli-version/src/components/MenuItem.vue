e39f4380c9d9ad517d15ce7eaa66ced352147997
Jérôme DUBOIS <jerome.dubois@gmail.com>
2021-08-16 13:07:56 +0200

    Hook cycle de vie

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,7 +1,27 @@
 <script>
 export default {
 	name: "MenuItem",
-	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"]
+	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+	data() {
+		return {
+			onSale: false
+		}
+	},
+	computed: {
+		generatedPrice() {
+			if (this.onSale) {
+				return (this.price * 0.9).toFixed(2)
+			} else {
+				return this.price
+			}
+		}
+	},
+	beforeMount() {
+		const today = new Date().getDate()
+		if (today % 2 === 0) {
+			this.onSale = true
+		}
+	}
 }
 </script>
 
@@ -10,7 +30,7 @@ export default {
 		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
 		<div>
 			<h3>{{ name }}</h3>
-			<p>Prix : {{ price }}</p>
+			<p>Prix : {{ generatedPrice }}</p>
 			<p v-if="inStock">En stock</p>
 			<p v-else>En rupture de stock</p>
 			<div>
